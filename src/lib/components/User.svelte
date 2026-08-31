b415cd766639b1b48c6e0d5a0efea4f8c1bda4b8
Thomas <timmy@fruchtwein.net>
2026-08-31 02:08:21 +0200

    add User component with avatar + name

---
+++ src/lib/components/User.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let { username, image } = $props();
+</script>
+
+
+<div class="flex justify-center items-center text-center gap-3 py-3 text-sm">
+    <img alt="{username}'s profile picture" class="h-9 w-9 rounded-full bg-white" src="{image}" />
+    {username}
+</div>
