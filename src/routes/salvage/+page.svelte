b043f8d4727a07ee5c99067a004e0c14f4b362a0
Thomas <timmy@fruchtwein.net>
2026-08-31 15:17:49 +0200

    draft overview

--- src/routes/salvage/+page.svelte
+++ src/routes/salvage/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
     import { getJoinedSalvageRuns } from "$lib/database/salvage.remote.ts";
     import User from "$lib/components/User.svelte"
+    import Button from "$lib/components/Button.svelte"
+    import Icon from "$lib/components/Icon.svelte";
+    import {faPlusCircle, faRocket} from "@fortawesome/free-solid-svg-icons";
 
     let { data } = $props();
     let salvageRuns = $derived(getJoinedSalvageRuns().current);
 </script>
-
-<h1>Salvage runs</h1>
-<p>Select which salvage run you want to continue.</p>
 
 <table class={[
     "w-full divide-y divide-gray-200 table-auto",
@@ -17,17 +17,6 @@
     "[&_tbody_tr]:h-10 [&_tbody_tr]:hover:bg-gray-50",
     "[&_td]:px-2 [&_td]:text-sm [&_td]:whitespace-nowrap [&_td]:text-center"
     ]}>
-
-    <!--
-    <colgroup>
-        <col/>
-        <col class="w-40"/>
-        <col class="w-40"/>
-        <col />
-        <col />
-        <col class="w-40"/>
-    </colgroup>
-    -->
 
     <thead>
     <tr>
@@ -43,12 +32,19 @@
     <tbody>
     {#each salvageRuns as sr}
         <tr>
-            <td>{sr.id}</td>
+            <td class="font-semibold">{sr.id}</td>
             <td><User username={sr.owner_name} image={sr.owner_image} /></td>
             <td>{sr.created_at.toLocaleString("en-GB", { timeZone: "UTC" })}</td>
             <td>{sr.member_count}</td>
             <td>{Intl.NumberFormat().format(sr.profit)} aUEC</td>
+            <td><Button class="w-full" href="/salvage/{sr.id}">Continue <Icon class="h-8 w-8 ml-3" icon={faRocket}/></Button></td>
         </tr>
     {/each}
     </tbody>
 </table>
+
+<!-- spacer -->
+<div class="h-1/2"></div>
+
+<!-- add button to open right side sidebar -->
+<Button class="p-2 rounded float-right"><Icon class="w-4 h-4" icon={faPlusCircle}/></Button>
